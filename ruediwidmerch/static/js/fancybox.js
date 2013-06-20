f40ae507133c9424474c2dadc7f2062546e5c1ac
diellordielli <dd@feinheit.ch>
2013-06-20 18:28:01 +0200

    fancybox

--- ruediwidmerch/static/js/fancybox.js
+++ ruediwidmerch/static/js/fancybox.js
@@ -6,25 +6,28 @@ $(document).ready(function() {
             activeFilter = $('#container').data('filter') || 'cartoon',
             $group = $('.' + activeFilter + ' a');
 
-        // Get on screen image
-        var screenImage = $this.find('img');
-
-        // Create new offscreen image to test
-        var theImage = new Image();
-        theImage.src = screenImage.attr("src");
-
-        // Get accurate measurements from that.
-        var imageWidth = theImage.width;
-        var imageHeight = theImage.height;
-
         $.fancybox($group, {
             type: 'ajax',
             index: $group.index(this),
             autoSize: false,
-            width: imageWidth,
-            height: imageHeight
+            afterLoad: function() {
+                var imgDimensions = getImageDimensions(this.element.find('img').attr('src'));
+
+                this.width = imgDimensions.width;
+            }
         });
        
     });
 
 });
+
+function getImageDimensions(src) {
+        // Create new offscreen image to test
+        var theImage = new Image();
+        theImage.src = src;
+
+        return {
+            width: theImage.width,
+            height: theImage.height
+        }
+}
